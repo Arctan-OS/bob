edc76e72821fcc06abaa6b57fd02f4d95089d848
awewsomegamer <awewsomegamer@gmail.com>
2026-03-20 00:12:16 -0500

    Fix evilness in license

--- bob.sh
+++ bob.sh
@@ -1,6 +1,6 @@
-#Copyright 2026 awewsomegamer <awewsomegamer@gmail.com>
+# Copyright 2026 awewsomegamer <awewsomegamer@gmail.com>
 #
-#Permission is hereby granted, free of charge,
+# Permission is hereby granted, free of charge,
 # to any person obtaining a copy of this software
 # and associated documentation files (the “Software”),
 # to deal in the Software without restriction,
